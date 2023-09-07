068830c658027bf51b2183a50143cf3f181c6e9c
sameermahemct <141718099+sameermahemct@users.noreply.github.com>
2023-09-06 16:46:24 -0800

    TEST COMMIT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { NgForm } from '@angular/forms'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TempForm2';
+
+  RegisterStudent(studentForm: NgForm): void {   
+    console.log(studentForm.value);
+
+
+
+
+    debugger;
+var firstName=studentForm.value.firstName;
+var lastName=studentForm.value.lastName;
+var FirstName=studentForm.value.email;
+ 
+  }
 }
